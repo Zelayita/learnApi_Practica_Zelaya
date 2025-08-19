26f47f28e3bb66a97a0388e01c8bafaccf5c897b
Zelayita <20240181@ricaldone.edu.sv>
2025-08-19 15:13:46 -0600

    Cambios para mi version

--- src/main/java/IntegracionBackFront/backfront/Services/Products/ProductService.java
+++ src/main/java/IntegracionBackFront/backfront/Services/Products/ProductService.java
@@ -56,6 +56,7 @@ public class ProductService {
         productoExistente.setFechaIngreso(json.getFechaIngreso());
         productoExistente.setCategoriaId(json.getCategoriaId());
         productoExistente.setUsuarioId(json.getUsuarioId());
+        productoExistente.setImagenurl(json.getImagenurl());
         //3. Actualización del registro
         ProductEntity productoActualizado = repo.save(productoExistente);
         //4. Convertir a DTO
@@ -83,6 +84,7 @@ public class ProductService {
         dto.setFechaIngreso(objEntity.getFechaIngreso());
         dto.setCategoriaId(objEntity.getCategoriaId());
         dto.setUsuarioId(objEntity.getUsuarioId());
+        dto.setImagenurl(objEntity.getImagenurl());
         return dto;
     }
 
@@ -95,6 +97,7 @@ public class ProductService {
         entity.setFechaIngreso(json.getFechaIngreso());
         entity.setCategoriaId(json.getCategoriaId());
         entity.setUsuarioId(json.getUsuarioId());
+        entity.setImagenurl(json.getImagenurl());
         return entity;
     }
 }
